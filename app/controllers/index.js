e4e978810dd37250fafd051ada70d7b636f83478
Vinicius Linck <vinnylinck@gmail.com>
2014-02-16 21:01:51 -0300

    CORS headers update

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -5,9 +5,9 @@ var pkg = require('../../package.json');
 
 exports.hit = function indexHit(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+
     next();
 };
 
