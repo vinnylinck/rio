d7e264b83170687ca30f0d86fe77e0c539caf55e
Vinicius Linck <vinnylinck@gmail.com>
2014-02-16 21:27:31 -0300

    Adding new headers to index route

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -5,8 +5,8 @@ var pkg = require('../../package.json');
 
 exports.hit = function indexHit(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    //res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Authorization, Accept');
     next();
 };
 
