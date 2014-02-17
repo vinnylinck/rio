0aa18a05517fe3c0cc50b205de90b96bb3899077
Vinicius Linck <vinnylinck@gmail.com>
2014-02-16 21:20:05 -0300

    Undoing index route changes.

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,8 +3,7 @@
 
 module.exports = function indexRoutes(app) {
     var index = require('../controllers/index');
-    
-    app.use(index.hit);
+
     app.all('*', index.hit);
     app.get('/', index.root);
 };
